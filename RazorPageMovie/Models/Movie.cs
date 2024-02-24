1ca38d9ece9e4bf090b28ae5a847031356f67f74
MarcosLV <zeta_marcos@hotmail.com>
2024-02-24 13:24:46 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿// DECLARACIÓN DE LIBRERIAS
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+// MODELO DE DESARROLLO APLICADO EN ESTE PROYECTO ES "MODEL FIRST"
+namespace RazorPageMovie.Models
+{
+    public class Movie
+    {
+        // EN LA DECLARACION DE VARIABLES EL ? DESPUÉS DEL NOMBRE DE LA VARIABLE INDICA QUE PUEDE SER NULL
+        public int Id { get; set; }
+        [StringLength(60,MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; }
+        // ANOTACIÓN DE TIPO DE DATO, PARA ESO SE UTILIZA EL USING DATAANNOTATIONS
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+    }
+}
---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿// DECLARACIÓN DE USING
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Plugins;
+using RazorPageMovie.Data;
+//using System.ComponentModel.DataAnnotations;
+
+namespace RazorPageMovie.Models
+{
+    public static class SeedData
+    {
+        // INCIALIZAR PARA INTRODUCIR DATOS A LA DB CUANDO DE EJECUTE EL PROYECTO
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentException("Null RazorPagesMovieContext");
+                }
+                // PARA MIRAR CUALQUIER PELICULA
+                if (context.Movie.Any()) 
+                {
+                    return; // LA DB MUESTRA TODO EL CONTENIDO DE LA CLASE
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry met Sally",
+                        ReleaseDate = DateTime.Parse("1982-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "GhostBusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "GhostBusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+        
+    }
+}
